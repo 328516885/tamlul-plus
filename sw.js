53904467ea67f236d16d6ecce17d1fc354346616
328516885 <ay0533115854@gmail.com>
2026-09-25 00:54:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker – תמלול פלוס */
-const VERSION = 'tplus-v1.3.0';
+const VERSION = 'tplus-v1.3.1';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
 const SHARE_CACHE = 'tplus-share';
 const LIB_CACHE = 'tplus-libs';
