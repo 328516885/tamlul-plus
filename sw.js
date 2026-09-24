46ad98b8e9f2d3f9404b151c1c6aed2e50e3ad1c
328516885 <ay0533115854@gmail.com>
2026-09-24 19:44:30 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-/* Service Worker – תמלול */
-const VERSION = 'tamlul-v1.0.0';
+/* Service Worker – תמלול פלוס */
+const VERSION = 'tplus-v1.0.1';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './apple-touch-icon.png'];
-const SHARE_CACHE = 'tamlul-share';
-const LIB_CACHE = 'tamlul-libs';
+const SHARE_CACHE = 'tplus-share';
+const LIB_CACHE = 'tplus-libs';
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(VERSION).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
@@ -11,7 +11,7 @@ self.addEventListener('install', (e) => {
 self.addEventListener('activate', (e) => {
   e.waitUntil((async () => {
     const keys = await caches.keys();
-    await Promise.all(keys.filter((k) => k !== VERSION && k !== SHARE_CACHE && k !== LIB_CACHE).map((k) => caches.delete(k)));
+    await Promise.all(keys.filter((k) => k.startsWith('tplus-') && k !== VERSION && k !== SHARE_CACHE && k !== LIB_CACHE).map((k) => caches.delete(k)));
     await self.clients.claim();
   })());
 });
